539407fffb7708e8745d2869d84ad82437df1dbc
jskdr <jamessungjin.kim@gmail.com>
2021-12-24 17:44:00 +0900

    rust

--- all_repository/rust_lab/autograd_study/src/main.rs
+++ all_repository/rust_lab/autograd_study/src/main.rs
@@ -1,24 +1,12 @@
 use autograd as ag;
 
 fn main() {
-    println!("Hello, world!");
-
     ag::with(|g: &mut ag::Graph<_>| {
         let x = g.placeholder(&[]);
-        let y = g.placeholder(&[]);
-        let z = 2.*x*x + 3.*y + 1.;
+        let y = (x + 1.)*(x + 1.);
      
-        // dz/dy
-        let gy = &g.grad(&[z], &[y])[0];
-        println!("{:?}", gy.eval(&[]));   // => Ok(3.)
-     
-        // dz/dx (requires to fill the placeholder `x`)
-        let gx = &g.grad(&[z], &[x])[0];
-        let feed = ag::ndarray::arr0(10.);
-        println!("{:?}", gx.eval(&[x.given(feed.view())]));  // => Ok(8.)
-     
-        // ddz/dx (differentiates `z` again)
-        let ggx = &g.grad(&[gx], &[x])[0];
-        println!("{:?}", ggx.eval(&[]));  // => Ok(4.)
+        let dy_dx = &g.grad(&[y], &[x])[0];
+        let x_val = ag::ndarray::arr0(1.);
+        println!("{:?}", dy_dx.eval(&[x.given(x_val.view())])); 
      });
 }
