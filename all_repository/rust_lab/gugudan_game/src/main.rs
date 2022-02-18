fef6193ad81643f06adfde09d8cd1e44acf74825
jskdr <jamessungjin.kim@gmail.com>
2022-02-18 22:05:33 +0900

    gugudan

---
+++ all_repository/rust_lab/gugudan_game/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    show_problem();
+    ask_answer();
+    provide_result();
+}
+
+fn show_problem() {
+    println!("Show problem");
+}
+
+fn ask_answer() {
+    println!("Ask answer");
+}
+
+fn provide_result() {
+    println!("Show problem");
+}
