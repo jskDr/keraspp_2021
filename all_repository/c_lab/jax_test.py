dcbb39d159a8eae887c22f570fdbaf5b739bc1f5
jskdr <jamessungjin.kim@gmail.com>
2022-07-10 00:25:45 +0900

    update

--- all_repository/c_lab/jax_test.py
+++ all_repository/c_lab/jax_test.py
@@ -1,6 +1,8 @@
+#%% importing libraries
 from jax import grad, random
 from jax import numpy as jnp
 
+#%% preparing data
 x = jnp.array([1.0, 2.0, 3.0])
 y = 2.0*x + 1.0
 
@@ -9,6 +11,7 @@ N = 2
 x_train, y_train, x_test, y_test = x[:N], y[:N], x[N:], y[N:]
 print(x_train, y_train, x_test, y_test)
 
+#%% define the model
 def model(w, b, x):
     return w*x + b
 
@@ -19,9 +22,9 @@ def loss(w, b, x, y):
 dloss_dw = grad(loss, argnums=0)
 dloss_db = grad(loss, argnums=1)
 
-# train model
+#%% train model
 w, b = 0.0, 0.0
-N_epoch = 1000
+N_epoch = 100
 for epoch in range(N_epoch):
     for x_i, y_i in zip(x_train, y_train):
         dl_dw = dloss_dw(w, b, x_i, y_i)
@@ -31,4 +34,5 @@ for epoch in range(N_epoch):
     l = loss(w, b, x_train, y_train)
     err = loss(w, b, x_test, y_test)
     if epoch % 10 == 0:
-        print(f'epoch {epoch}: test loss {err:.3e}, train loss {l:.3e}, w {w:.3f}, b {b:.3f}')
+        print(f'epoch {epoch}: test loss {err:.3e}, train loss {l:.3e}, w {w:.2f}, b {b:.2f}')
+# %%
