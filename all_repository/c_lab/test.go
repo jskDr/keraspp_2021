b2927b814b4d2ed8507e829e47a272a53fb786a3
jskdr <jamessungjin.kim@gmail.com>
2022-07-11 13:33:32 +0900

    go code is developed by codepilot

--- all_repository/c_lab/test.go
+++ all_repository/c_lab/test.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func sum_of_vector(vector []int) int {
+	sum := 0
+	for i, v := range vector {
+		fmt.Println(i, ": ", v)
+		sum += v
+	}
+	return sum
+}
+
 func main() {
 	fmt.Println("hello world")
+
+	// initialize vector
+	vector := []int{10, 20, 30, 40, 50}
+	sum := sum_of_vector(vector)
+	fmt.Println(sum) // it should be 15.
 }
