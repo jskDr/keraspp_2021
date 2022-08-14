4932d9f5a9c7aeb2a6d7a58a094c589b4a1735d1
jskdr <jamessungjin.kim@gmail.com>
2022-08-15 00:11:26 +0900

    rust gugu

--- all_repository/rust_lab/gugudan_game/src/main.rs
+++ all_repository/rust_lab/gugudan_game/src/main.rs
@@ -1,17 +1,37 @@
+use rand::Rng; // 0.8.5
+use std::io; 
+
 fn main() {
-    show_problem();
-    ask_answer();
-    provide_result();
+    println!("GuGuDan Game");
+    let (a, b) = show_problem();
+    let answer = ask_answer();
+    provide_result(a, b, answer);
 }
 
-fn show_problem() {
-    println!("Show problem");
+fn show_problem() -> (i32, i32) {
+    // print without new line
+    println!("Answer to this problem: ");
+    let a = rand::thread_rng().gen_range(1..10);
+    let b = rand::thread_rng().gen_range(1..10);
+    println!("{} x {} = ? ", a, b);
+    return (a, b);
 }
 
-fn ask_answer() {
-    println!("Ask answer");
+fn ask_answer() -> i32 {
+    let mut answer = String::new();
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("Failed to read line");
+    // convert Str to int
+    return answer.trim().parse::<i32>().unwrap();
 }
 
-fn provide_result() {
-    println!("Show problem");
+fn provide_result(a: i32, b: i32, answer: i32) {
+    println!("Answer: {} x {} = {}", a, b, a * b);
+    print!("Your answer of {} is ", answer);
+    if answer == a * b {
+        println!("Correct!");
+    } else {
+        println!("Wrong!");
+    }
 }
